69e1d7aa2ad81b34fbe9d5b6da2ec66e4016d6d5
isola-23 <yuka.shimamura@picappinc.jp>
2023-11-28 12:00:23 +0900

    fix

---
+++ main.js
@@ -0,0 +1,5 @@
+window.addEventListener('DOMContentLoaded',()=>{
+    const t=document.querySelectorAll(':not(:defined)');
+    for(const e of t) {e.style.visibility="hidden";}; 
+    window.spearly.config.AUTH_KEY="r2vIw5NeSbh_7hKwsKG668qrHJVh4RoAKqgguxcEVgw"
+},{once:true})
